ea98c42d7f434fdf7b4d1900db2b87d5d11d523e
sergiooval <sergio_val_89@hotmail.com>
2026-06-07 20:57:12 -0700

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,65 @@
 const TEAMS = [
- ['BRA','Brasil'],['MAR','Marruecos'],['HAI','Haití'],['SCO','Escocia'],['USA','Estados Unidos'],['PAR','Paraguay'],['AUS','Australia'],['TUR','Türkiye'],['GER','Alemania'],['CUW','Curaçao'],['CIV','Côte d’Ivoire'],['ECU','Ecuador'],['NED','Países Bajos'],['JPN','Japón'],['SWE','Suecia'],['TUN','Túnez'],['BEL','Bélgica'],['EGY','Egipto'],['IRN','Irán'],['NZL','Nueva Zelanda'],['ESP','España'],['CPV','Cabo Verde'],['KSA','Arabia Saudita'],['URU','Uruguay'],['FRA','Francia'],['SEN','Senegal'],['IRQ','Iraq'],['NOR','Noruega'],['ARG','Argentina'],['ALG','Argelia'],['AUT','Austria'],['JOR','Jordania'],['POR','Portugal'],['COD','Congo DR'],['UZB','Uzbekistán'],['COL','Colombia'],['ENG','Inglaterra'],['CRO','Croacia'],['GHA','Ghana'],['PAN','Panamá'],
+ ['MEX','México'],
+ ['RSA','Sudáfrica'],
+ ['KOR','Corea Republic'],
+ ['CZE','Chequia'],
+
+ ['CAN','Canadá'],
+ ['BIH','Bosnia-Herzegovina'],
+ ['QAT','Qatar'],
+ ['SUI','Suiza'],
+
+ ['BRA','Brasil'],
+ ['MAR','Marruecos'],
+ ['HAI','Haití'],
+ ['SCO','Escocia'],
+
+ ['USA','Estados Unidos'],
+ ['PAR','Paraguay'],
+ ['AUS','Australia'],
+ ['TUR','Türkiye'],
+
+ ['GER','Alemania'],
+ ['CUW','Curaçao'],
+ ['CIV','Côte d’Ivoire'],
+ ['ECU','Ecuador'],
+
+ ['NED','Países Bajos'],
+ ['JPN','Japón'],
+ ['SWE','Suecia'],
+ ['TUN','Túnez'],
+
+ ['BEL','Bélgica'],
+ ['EGY','Egipto'],
+ ['IRN','Irán'],
+ ['NZL','Nueva Zelanda'],
+
+ ['ESP','España'],
+ ['CPV','Cabo Verde'],
+ ['KSA','Arabia Saudita'],
+ ['URU','Uruguay'],
+
+ ['FRA','Francia'],
+ ['SEN','Senegal'],
+ ['IRQ','Iraq'],
+ ['NOR','Noruega'],
+
+ ['ARG','Argentina'],
+ ['ALG','Argelia'],
+ ['AUT','Austria'],
+ ['JOR','Jordania'],
+
+ ['POR','Portugal'],
+ ['COD','Congo DR'],
+ ['UZB','Uzbekistán'],
+ ['COL','Colombia'],
+
+ ['ENG','Inglaterra'],
+ ['CRO','Croacia'],
+ ['GHA','Ghana'],
+ ['PAN','Panamá'],
 ];
 const SPECIALS = [
  {code:'CC', name:'Coca-Cola', count:14},
- {code:'FWC', name:'FIFA World Cup History', numbers:[9,10,11,12,13,14,15,16,17,19]}
+ {code:'FWC', name:'FIFA World Cup History', numbers:[9,10,11,12,13,14,15,16,17,18,19]}
 ];
