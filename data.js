4e922cb0b74048d3a75b504f07d8e6e804539553
sergiooval <sergio_val_89@hotmail.com>
2026-06-07 21:21:27 -0700

    Update data.js

--- data.js
+++ data.js
@@ -60,6 +60,6 @@ const TEAMS = [
  ['PAN','Panamá'],
 ];
 const SPECIALS = [
- {code:'CC', name:'Coca-Cola', count:14},
- {code:'FWC', name:'FIFA World Cup History', numbers:[9,10,11,12,13,14,15,16,17,18,19]}
+  {code:'FWC', name:'FIFA World Cup', numbers:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]},
+  {code:'CC', name:'Coca-Cola', count:14}
 ];
